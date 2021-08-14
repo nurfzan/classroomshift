e57d526e5ae5cec0090f3b484743f4bc0a22ca4c
Ghany AE <ghany.aersa@fifgroup.astra.co.id>
2021-08-15 01:11:14 +0700

    init frontend

---
+++ components/partials/sidebar.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="main-sidebar">
+    <aside id="sidebar-wrapper">
+      <div class="sidebar-brand">
+        <nuxt-link to="/">Fullstack Academy</nuxt-link>
+      </div>
+      <div class="sidebar-brand sidebar-brand-sm">
+        <nuxt-link to="/">FA</nuxt-link>
+      </div>
+      <ul class="sidebar-menu">
+        <li v-for="(menu,i) in menus" :key="i">
+          <nuxt-link :to="menu.link" class="nav-link">
+            <i :class="menu.icon" style="font-size:1.2em"></i> <span>{{menu.name}}</span>
+          </nuxt-link>
+        </li>
+      </ul>
+    </aside>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      menus: [
+        { icon: "fab fa-dashcube", name: "All Class", link: "/" },
+      ],
+    };
+  },
+};
+</script>
